2d7df58779c97f3490d43fd048d4d921c9132c53
freelans0604 <account@freelans.net>
2017-01-22 00:27:51 +0900

    add new file

---
+++ wp-cast-press.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+Plugin Name: WP CastPress
+Plugin URI: https://freelans.net/
+Description: キャスト管理用プラグイン。キャストの基本情報やスケジュール、オプションを設定・管理できます。
+Version: 1.0.1
+Author: Hidenori Yasutake
+Author URI: http://white-zip.com
+License: GPL2
+
+Copyright 2017 WP CastPress (email : yasutake@freelans.net)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+
+
+
+
+
+include_once(dirname(__FILE__) . "/inc/post_options.php");
+include_once(dirname(__FILE__) . "/inc/wp-ajax.php");
+include_once(dirname(__FILE__) . "/inc/output.php");
+include_once(dirname(__FILE__) . "/inc/wpcp_functions.php");
+include_once(dirname(__FILE__) . "/inc/wpcp_admin.php");
+
+
+
+
+
+
+?>
